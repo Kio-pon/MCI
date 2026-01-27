9d3a87509dac23ca674c68367af10c66c04e72c8
Azyan <azyanahmed1@gmail.com>
2026-01-27 12:26:27 +0500

    Lab03_completed

---
+++ MCI-Lab03/build/CMakeFiles/MCI_Lab03.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/azyan/MCI (Copy)/MCI-Lab03/startup_stm32f303xc.s" "/home/azyan/MCI (Copy)/MCI-Lab03/build/CMakeFiles/MCI_Lab03.dir/startup_stm32f303xc.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F303xC"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/azyan/MCI (Copy)/MCI-Lab03/cmake/stm32cubemx/../../Core/Inc"
+  "/home/azyan/MCI (Copy)/MCI-Lab03/cmake/stm32cubemx/../../Drivers/STM32F3xx_HAL_Driver/Inc"
+  "/home/azyan/MCI (Copy)/MCI-Lab03/cmake/stm32cubemx/../../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy"
+  "/home/azyan/MCI (Copy)/MCI-Lab03/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32F3xx/Include"
+  "/home/azyan/MCI (Copy)/MCI-Lab03/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/azyan/MCI (Copy)/MCI-Lab03/Core/Src/main.c" "CMakeFiles/MCI_Lab03.dir/Core/Src/main.c.o" "gcc" "CMakeFiles/MCI_Lab03.dir/Core/Src/main.c.o.d"
+  "/home/azyan/MCI (Copy)/MCI-Lab03/Core/Src/stm32f3xx_hal_msp.c" "CMakeFiles/MCI_Lab03.dir/Core/Src/stm32f3xx_hal_msp.c.o" "gcc" "CMakeFiles/MCI_Lab03.dir/Core/Src/stm32f3xx_hal_msp.c.o.d"
+  "/home/azyan/MCI (Copy)/MCI-Lab03/Core/Src/stm32f3xx_it.c" "CMakeFiles/MCI_Lab03.dir/Core/Src/stm32f3xx_it.c.o" "gcc" "CMakeFiles/MCI_Lab03.dir/Core/Src/stm32f3xx_it.c.o.d"
+  "/home/azyan/MCI (Copy)/MCI-Lab03/Core/Src/syscalls.c" "CMakeFiles/MCI_Lab03.dir/Core/Src/syscalls.c.o" "gcc" "CMakeFiles/MCI_Lab03.dir/Core/Src/syscalls.c.o.d"
+  "/home/azyan/MCI (Copy)/MCI-Lab03/Core/Src/sysmem.c" "CMakeFiles/MCI_Lab03.dir/Core/Src/sysmem.c.o" "gcc" "CMakeFiles/MCI_Lab03.dir/Core/Src/sysmem.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MCI-Lab03/build/CMakeFiles/MCI_Lab03.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "MCI_Lab03.map"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/main.c.o"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/main.c.o.d"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/stm32f3xx_hal_msp.c.o"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/stm32f3xx_hal_msp.c.o.d"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/stm32f3xx_it.c.o"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/stm32f3xx_it.c.o.d"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/syscalls.c.o"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/syscalls.c.o.d"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/sysmem.c.o"
+  "CMakeFiles/MCI_Lab03.dir/Core/Src/sysmem.c.o.d"
+  "CMakeFiles/MCI_Lab03.dir/startup_stm32f303xc.s.o"
+  "MCI_Lab03"
+  "MCI_Lab03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/MCI_Lab03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
