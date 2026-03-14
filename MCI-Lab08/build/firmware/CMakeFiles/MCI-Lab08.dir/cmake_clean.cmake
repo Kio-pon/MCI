f0aed53ac256f6e56034000a71b195e1c62666c0
Azyan <azyanahmed1@gmail.com>
2026-03-14 09:48:14 +0500

    Lab08 Completed

---
+++ MCI-Lab08/build/firmware/CMakeFiles/MCI-Lab08.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "MCI-Lab08.map"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/main.c.obj"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/main.c.obj.d"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/stm32f3xx_hal_msp.c.obj"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/stm32f3xx_hal_msp.c.obj.d"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/stm32f3xx_it.c.obj"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/stm32f3xx_it.c.obj.d"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/syscalls.c.obj.d"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/MCI-Lab08.dir/Core/Src/sysmem.c.obj.d"
+  "CMakeFiles/MCI-Lab08.dir/startup_stm32f303xc.s.obj"
+  "MCI-Lab08.elf"
+  "MCI-Lab08.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/MCI-Lab08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MCI-Lab08/build/firmware/CMakeFiles/MCI-Lab08.dir/startup_stm32f303xc.s.d
@@ -0,0 +1,2 @@
+CMakeFiles/MCI-Lab08.dir/startup_stm32f303xc.s.obj: \
+ /home/azyan/MCI_Copy/MCI-Lab08/startup_stm32f303xc.s
